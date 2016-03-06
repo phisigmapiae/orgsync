87d959401a8eeb0411fdf71d6f9154ec41d54765
piotrromanowski <piotrromanowski1@gmail.com>
2016-03-06 02:14:43 -0500

    Test Commit

--- src/views/base.js
+++ src/views/base.js
@@ -24,22 +24,21 @@ export default class Base extends Component {
         </div>
 
         <div className="navigation_wrapper">
-        <div className="navigation">
-        <ul>
-        	<li><a href="http://wcu.orgsync.com/org/phisigmapi/home" >Home</a></li>
-        	<li><a href="http://wcu.orgsync.com/org/phisigmapi/About">About</a></li>
-        	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Calendar">Calendar</a></li>
-        	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Become_a_Brother">Become a Brother</a></li>
-        	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Active_Brothers">Active Brothers</a></li>
-        	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Initiates">Initiates</a></li>
-        	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Alumni_Spotlight">Alumni Spotlight</a></li>
-        	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Contact_Us">Contact Us</a></li>
-        </ul>
-        </div>
+          <div className="navigation">
+            <ul>
+            	<li><a href="http://wcu.orgsync.com/org/phisigmapi/home" >Home</a></li>
+            	<li><a href="http://wcu.orgsync.com/org/phisigmapi/About">About</a></li>
+            	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Calendar">Calendar</a></li>
+            	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Become_a_Brother">Become a Brother</a></li>
+            	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Active_Brothers">Active Brothers</a></li>
+            	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Initiates">Initiates</a></li>
+            	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Alumni_Spotlight">Alumni Spotlight</a></li>
+            	<li><a href="http://wcu.orgsync.com/org/phisigmapi/Contact_Us">Contact Us</a></li>
+            </ul>
+          </div>
         </div>
 
         Base
-
         {this.props.children}
       </div>
     );
